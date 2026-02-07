2c3006e172247a2aa24afae3c5d12bb6dce9c718
Shilimed <mohamed.shili@fst.utm.tn>
2026-02-07 07:34:02 -1200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,9 @@
+/**
+ * BASE DE DONNÉES DES INSCRIPTIONS
+ * Structure : "ID_PROJET": ["Étudiant 1", "Étudiant 2", "Étudiant 3"]
+ */
+const inscriptionsGlobales = {
+    // Pour l'instant, aucun projet n'est occupé.
+    // Dès qu'un groupe est validé, ajoute-le ici comme ceci :
+    // "1": ["Ahmed Ben Ali", "Sonia Brahim", "Mohamed Aziz"]
+};
